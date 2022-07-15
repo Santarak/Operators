eafff1344987e0d41c08d6e9b1aada6d89c5ed9b
sanda <sanda@Santhosh>
2022-07-15 11:26:45 +0530

    un

---
+++ section5/src/operators/arithmeticOperators.java
@@ -0,0 +1,20 @@
+package operators;
+
+public class arithmeticOperators {
+
+	public static void main(String[] args) {
+		int a=9,b=8;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.println(a++);
+		System.out.println(a);
+		System.out.println(b--);
+		System.out.println(b);
+		
+
+	}
+
+}
